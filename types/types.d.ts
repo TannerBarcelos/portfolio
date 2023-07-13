26782716e8cefd3cb69d6d8bc25344daed854351
Tanner Barcelos <41309200+TannerBarcelos@users.noreply.github.com>
2023-07-13 00:12:23 -0700

    Update types.d.ts
    Adding back image type + adding decoupled types and interfaces

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,40 +1,46 @@
-import type { StaticImageData } from 'next/image'
-
-export interface TestimonialProps {
-  data: {
+type TestimonialDataType = {
     name: string
     position: string
     recommendation: string
     linkedin: string
-  }
+}
+export interface TestimonialProps {
+  data: TestimonialDataType
 }
 
-export interface TechItemProps {
-  icon: {
+type IconType = {
     path: string
     name: string
-  }
+}
+export interface TechItemProps {
+  icon: IconType
 }
 
-type TechType = string[]
-
-export interface ProjectProps {
-  project: {
+type TechnologiesType = Array<string>
+type Project = {
     id: string
     title: string
     link: string
     code_link: string
-    technologies: TechType
+    technologies: TechnologiesType
     description: string
     image: string
     btnText: string
-  }
+}
+export interface ProjectProps {
+  project: Project
 }
 
-export type IconArray = Array<{
-  name: string
-  path: string
-}>
+type ThemedImgType = {
+    dark: string,
+    light: string
+}
+export interface ImageMapType {
+  [imgName: string]: ThemedImgType
+}
+
+
+export type IconArray = Array<IconType>
 
 export type LinkType = {
   url: string
