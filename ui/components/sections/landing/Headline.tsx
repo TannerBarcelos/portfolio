0ab42ced77a55a2a869ca45b32d479028ce4c906
Tanner Barcelos <41309200+TannerBarcelos@users.noreply.github.com>
2023-12-11 21:40:00 -0800

    Update Headline.tsx

--- ui/components/sections/landing/Headline.tsx
+++ ui/components/sections/landing/Headline.tsx
@@ -2,7 +2,7 @@ const Headline = () => {
     return (
         <div className='headline' aria-label='Landing page headline section'>
             <h2>Tanner Barcelos</h2>
-            <p>Software Engineer. Aspiring Product Designer. Passionate about the web and UX.</p>
+            <p>Software Engineer. Aspiring Product Designer. Passionate about the Web, User Experience and Product Design</p>
         </div>
     )
 }
