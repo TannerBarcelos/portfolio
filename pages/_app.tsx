c0316810cf9a51278e69234e4f37a64e2bdd1bd3
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-01-20 11:00:20 -0800

    Adding telemetry collector

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/ui/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component { ...pageProps } />
+  return <>
+    <Component { ...pageProps } />
+    <SpeedInsights />
+  </>
 }
 
 export default MyApp
